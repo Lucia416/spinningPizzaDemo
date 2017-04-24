99e9e85607908d50f94ce1c7455030c9aa91e78e
Lucia416 <lucia@lucia3d.com>
2017-04-24 04:59:08 -0400

    bug fixed now time to polish

--- jquery.fortune.js
+++ jquery.fortune.js
@@ -8,10 +8,9 @@
     var options = $.extend({}, {
       prices: args,
       duration: 1000,
-      separation: 16,
+      separation: 2,
       min_spins: 10,
-      max_spins: 12,
-      clockWise: true,
+      max_spins: 15,
       onSpinBounce: function() {}
     }, args);
 
--- public/script/index.js
+++ public/script/index.js
@@ -1,84 +1,111 @@
-var prices = [
-  {
-    name: 'winThree',
-    id:'sliceThree'
-  },
-  {
-      name: 'loseTwo',
-      id:'sliceTwo'
-    },
-  {
-      name: 'winOne',
-      id:'sliceOne'
-    },
-  {
-      name: 'loseEight',
-      id:'sliceEight'
-    },
-  {
-      name: 'winSeven',
-      id:'sliceSeven'
-    },
-  {
-      name: 'loseSix',
-      id:'sliceSix'
-    },
-  {
-      name: 'winFive',
-      id:'sliceFive'
-    },
-  {
-      name: 'loseFour',
-      id:'sliceFour'
-    }
-];
 
 $(function() {
+  var prices = [
+    {
+      name: 'winThree',
+      id:'Three'
+    },
+    {
+        name: 'loseTwo',
+        id:'Two'
+      },
+    {
+        name: 'winOne',
+        id:'One'
+      },
+    {
+        name: 'loseEight',
+        id:'Eight'
+      },
+    {
+        name: 'winSeven',
+        id:'Seven'
+      },
+    {
+        name: 'loseSix',
+        id:'Six'
+      },
+    {
+        name: 'winFive',
+        id:'Five'
+      },
+    {
+        name: 'loseFour',
+        id:'Four'
+      }
+  ];
+
 
 var $r = $('.pizza').fortune(prices);
-$('#congrats').hide();
-$('#tryAgain').hide();
 
-var clickHandler = function() {
+  const userWinner = (e) => {
+  return `
+    <div class="title">
+      <span >You won a free pizza slice!!!</span>
+    </div>
+  `;
+}
+const userLoser = (e) => {
+return `
+  <div class="title">
+    <span > Sorry, spin again!</span>
+  </div>
+`;
+}
 
-  $r.spin().done(function(price) {
-    if(price.id === 'sliceOne'){
-      $('#congrats').show(600);
+
+
+var clickHandler = () => {
+
+  $r.spin().done((price)=>{
+
+    if(price.id === 'One'){
+      $('.title').html(userWinner);
       $('#sliceOne').hide();
       $('#plate').show(600);
-    } else if (price.id === 'sliceTwo'){
-      $('#tryAgain').show(600);
-    }else if (price.id === 'sliceThree'){
-      $('#congrats').show(600);
-        $('#sliceThree').css('-webkit-transform','translateZ(500px)');
-        $('#plate').show(600);
-    }else if (price.id === 'sliceFour'){
-      $('#tryAgain').show(600);
-      $('#sliceFour').css('-webkit-transform','translateZ(500px)');
-    }else if (price.id === 'sliceFive'){
-      $('#congrats').show(600);
-      $('#sliceFive').css('-webkit-transform','translateZ(500px)');
-            $('#plate').show(600);
-    }else if (price.id === 'sliceSix'){
-      $('.price').text('You have: ' + price.name);
-      $('#sliceSix').css('-webkit-transform','translateZ(500px)');
-    }else if (price.id === 'sliceSeven'){
-      $('#congrats').show(600);
-      $('#sliceSeven').css('-webkit-transform','translateZ(500px)');
-            $('#plate').show(600);
-    }else if (price.id == 'sliceEight'){
-      $('.price').text('You have: ' + price.name);
-      $('#sliceEight').css('-webkit-transform','translateZ(500px)');
+      setTimeout(() => {
+        $('#sliceOne').show();
+        $('#plate').hide(600);
 
-    }
-    $('.spinner').on('click touchstart', clickHandler);
+      }, 2000);
+    }else if (price.id === 'Four'){
+      $('.title').html(userWinner);
+      $('#sliceFour').hide();
+      $('#plate').show(600);
+      setTimeout(() => {
+        $('#sliceFour').show();
+        $('#plate').hide(600);
 
+      },2000);
+      }else if (price.id === 'Seven'){
+          $('.title').html(userWinner);
+          $('#sliceSeven').hide();
+          $('#plate').show(600);
+          setTimeout(() => {
+            $('#sliceSeven').show();
+            $('#plate').hide(600);
+
+          }, 2000);
+        }else{
+          $('.title').html(userLoser);
+          $('.pizza').show();
+        }
 
 
   });
+
 };
 
+
+
+
+
 $('.spinner').on('click touchstart', clickHandler);
+
+
+
+
+
 
 
 });
